527a39e4703bba0bb0f1e26311b724a51380f3be
Caleb Nordgren <cdn841@gmail.com>
2019-01-10 10:48:34 -0600

    Added ability to create blog posts

--- public/posts/index.php
+++ public/posts/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require '../../config/keys.php';
+
 require '../../core/db_connect.php';
 $meta=[];
 $meta['title']="Read my words, please";
@@ -9,7 +11,9 @@ $stmt = $pdo->query('SELECT * FROM posts');
 $content = "<h1>Read my words, please</h1>";
 
 while($row = $stmt->fetch()) {
-  $content .= "<a href=\"posts/view.php?slug={$row['slug']}\">{$row['title']}</a>";
+  $content .= "<div><a href=\"posts/view.php?slug={$row['slug']}\">{$row['title']}</a></div>";
 }
+
+$content .= "<br><br><hr><div><a href=\"posts/add.php\">Add New Post</a></div>";
 
 require '../../core/layout.php';
